1aaa03d602aeb5f0b55aeb008e86b4a1270d514a
Mark Loveday <maloveday@googlemail.com>
2013-05-08 19:07:10 +0000

    Initial Version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '719e0fee3cbb33f888f7f122219d87ce10527c7374d5633b6f02922455413e4273a6fd82414c0bfee972825bb83fc3acad400329132aeff7f91a78eb6b050efa'
